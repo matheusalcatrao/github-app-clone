ffc19c9b878b7487b7a8c1f9c84b4f2c2c591f17
Matheus Alcatrão <matheusalcatrao@hotmail.com>
2022-04-19 21:45:26 -0300

    feat: home layout

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect} from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, Image, Alert } from 'react-native';
+import api from './apiGithub';
 
 export default function App() {
+  const [repositores, setRepositores] = useState([]);
+
+
+  const _loadingRepositores = async () => {
+    try {
+      const response = await api.get('/users');
+      setRepositores(response.data)
+
+    } catch (error) {
+      console.error('Error on _loadingRepositores', error)
+    } 
+  }
+  const _viewDetails = async (userLogin) => {
+    try {
+      const response = await api.get(`/users/${userLogin}/repos`);
+      const followers = response.data.length;
+      
+      Alert.alert(userLogin, `Repositórios: ${followers}`);
+    } catch (error) {
+      console.error('Error on _viewDetails', error)
+    }
+  }
+
+  useEffect(() => {
+    _loadingRepositores();
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Repositórios do GitHub</Text>
+      <FlatList data={repositores} keyExtractor={item => item.id} renderItem={({item}) => (
+        <TouchableOpacity onPress={() => _viewDetails(item.login)} style={styles.line} >
+          <Image style={styles.image} source={{ uri: item.avatar_url}} />
+          <Text style={styles.lineText}>{item.login}</Text>
+        </TouchableOpacity>
+      )} />
     </View>
   );
 }
@@ -13,8 +46,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#6e5494',
   },
+  title: {
+    color: '#fff',
+    fontSize: 20,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginTop: 65
+    
+  },
+  line: {
+    flexDirection: 'row',
+    alignSelf: 'center',
+    alignItems: 'center',
+    width: '80%',
+    height: 60,
+    backgroundColor: '#f5f5f5',
+    marginTop: 30,
+  },
+  lineText: {
+    color: '#333',
+    fontSize: 20,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontStyle: 'italic'
+  },
+  image: {
+    width: 35,
+    height: 35,
+    borderRadius: 20,
+    marginLeft: 15,
+    marginRight: 15
+  }
 });
